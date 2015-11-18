0195a4cef097257857d65a711ea655ed840addfb
Paul Carduner <paul@carduner.net>
2015-11-17 23:08:24 -0800

    make npm run compile also build a single minified file in the lib directory

---
+++ webpack-package.config.js
@@ -0,0 +1,27 @@
+var path = require("path")
+var webpack = require('webpack')
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    "wescheme-js.min": './src/wescheme.js'
+  },
+  output: {
+    path: path.resolve(__dirname, "lib"),
+    filename: "[name].js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+      { test: /\.css$/, loaders: ["style", "css"] }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ]
+}
