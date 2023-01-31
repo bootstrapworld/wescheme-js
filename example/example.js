e1e19780ba380f5dcf7144cb5bc47cad5349300a
Adam Solove <asolove@gmail.com>
2023-01-30 19:44:47 -0500

    Add minimal PoC of reading files from Drive.

--- example/example.js
+++ example/example.js
@@ -155,6 +155,32 @@ window.handleAuthClick = function() {
   }
 }
 
+  /**
+   * Print metadata for first 10 files.
+   */
+    async function listFiles() {
+    let response;
+    try {
+      response = await gapi.client.drive.files.list({
+        'pageSize': 10,
+        'fields': 'files(id, name)',
+      });
+    } catch (err) {
+      document.getElementById('content').innerText = err.message;
+      return;
+    }
+    const files = response.result.files;
+    if (!files || files.length == 0) {
+      document.getElementById('content').innerText = 'No files found.';
+      return;
+    }
+    // Flatten to string to display
+    const output = files.reduce(
+        (str, file) => `${str}${file.name} (${file.id})\n`,
+        'Files:\n');
+    document.getElementById('files').innerText = output;
+  }
+
 /**
  *  Sign out the user upon button click.
  */
